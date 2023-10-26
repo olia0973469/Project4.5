e290c47c7a7eff585d35733eeef9656bda0ca46c
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2023-10-26 13:27:53 +0300

    Add project files.

---
+++ Project4.5/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if ((y <= R && y >= -x && y >= 0) ||
+			(y >= -R && y <= x && y <= 0) ||
+			(y * y <= R * R - x * x && -y >= -x && x <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
